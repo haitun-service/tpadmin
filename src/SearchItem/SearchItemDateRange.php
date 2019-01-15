78cfa044d53a353562390a1f633ee314cf6d9f9e
刘一二 <liuguoqiang@haitun.hk>
2019-01-15 17:19:27 +0800

    日期检索条件增加时间范围限制

--- src/SearchItem/SearchItemDateRange.php
+++ src/SearchItem/SearchItemDateRange.php
@@ -11,6 +11,8 @@ class SearchItemDateRange extends Driver
 
     protected $defaultStartDate = null;
     protected $defaultEndDate = null;
+    protected $minStartDate = null;
+    protected $maxEndDate = null;
     protected $theme = '#393D49';
 
     /**
@@ -29,6 +31,14 @@ class SearchItemDateRange extends Driver
 
         if (isset($params['defaultEndDate'])) {
             $this->defaultEndDate = $params['defaultEndDate'];
+        }
+
+        if (isset($params['minStartDate'])) {
+            $this->minStartDate = $params['minStartDate'];
+        }
+
+        if (isset($params['maxEndDate'])) {
+            $this->maxEndDate = $params['maxEndDate'];
         }
 
         if (isset($params['theme'])) {
@@ -66,8 +76,16 @@ class SearchItemDateRange extends Driver
 
         $html .= '<script>';
         $html .= '$(document).ready(function(){';
-        $html .= 'laydate.render({elem: \'#' . $this->key . '_start_date\',theme: \'' . $this->theme . '\'});';
-        $html .= 'laydate.render({elem: \'#' . $this->key . '_end_date\',theme: \'' . $this->theme . '\'});';
+        $html .= 'laydate.render({elem: \'#' . $this->key . '_start_date\',theme: \'' . $this->theme . '\'';
+        if ($this->minStartDate !== null) {
+            $html .= ',min:\'' . $this->minStartDate . '\'';
+        }
+        $html .= '});';
+        $html .= 'laydate.render({elem: \'#' . $this->key . '_end_date\',theme: \'' . $this->theme . '\'';
+        if ($this->maxEndDate !== null) {
+            $html .= ',max:\'' . $this->maxEndDate . '\'';
+        }
+        $html .= '});';
         $html .= '})';
         $html .= '</script>';
 
@@ -81,15 +99,44 @@ class SearchItemDateRange extends Driver
 
         $startDate = null;
         if (isset($condition[$this->key . '_start_date']) && $condition[$this->key . '_start_date']) {
+
             $startDate = trim($condition[$this->key . '_start_date']);
+
+            if ($this->minStartDate !== null) {
+                if (strtotime($startDate) < strtotime($this->minStartDate)) {
+                    $startDate = $this->minStartDate;
+                }
+            }
+
             $where = ' ' . ($this->table === null ? '' : ('`' . $this->table . '`.')) . '`' . $this->key . '`>=\'' . $startDate . ' 00:00:00\'';
+        }
+
+        if (!$startDate) {
+            if ($this->minStartDate !== null) {
+                $startDate = $this->minStartDate;
+                $where = ' ' . ($this->table === null ? '' : ('`' . $this->table . '`.')) . '`' . $this->key . '`>=\'' . $startDate . ' 00:00:00\'';
+            }
         }
 
         $endDate = null;
         if (isset($condition[$this->key . '_end_date']) && $condition[$this->key . '_end_date']) {
             $endDate = trim($condition[$this->key . '_end_date']);
+
+            if ($this->maxEndDate !== null) {
+                if (strtotime($endDate) > strtotime($this->maxEndDate)) {
+                    $endDate = $this->maxEndDate;
+                }
+            }
+
             if ($where) $where .= ' AND';
             $where .= ' ' . ($this->table === null ? '' : ('`' . $this->table . '`.')) . '`' . $this->key . '`<=\'' . $endDate . ' 23:59:59\'';
+        }
+
+        if (!$endDate) {
+            if ($this->maxEndDate !== null) {
+                $endDate = $this->maxEndDate;
+                $where .= ' ' . ($this->table === null ? '' : ('`' . $this->table . '`.')) . '`' . $this->key . '`<=\'' . $endDate . ' 23:59:59\'';
+            }
         }
 
         return $where;
